f793907b183a906ed54a8368bc03c2c30129701e
Vinay Ganesh <vinay.ganesh@icloud.com>
2017-09-06 22:13:33 -0700

    added command to clean and build before testing

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 
-xcodebuild test -project CleanViewLayout.xcodeproj -scheme CleanViewLayout -destination 'platform=iOS Simulator,name=iPhone 7 Plus,OS=10.3.1'
-slather coverage -s --scheme CleanViewLayout CleanViewLayout.xcodeproj
+xcodebuild clean build test -project CleanViewLayout.xcodeproj -scheme CleanViewLayout -destination 'platform=iOS Simulator,name=iPhone 7 Plus,OS=10.3.1'
+slather coverage --html --scheme CleanViewLayout CleanViewLayout.xcodeproj
