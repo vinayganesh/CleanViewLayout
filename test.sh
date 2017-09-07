2c745df5a6dc9d2c7146a9f709e1085a187edffb
Vinay Ganesh <vinay.ganesh@icloud.com>
2017-09-06 23:06:55 -0700

    Added Pod file, Added swift lint run script, updated slather command for workspace and html generation.

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 
-xcodebuild clean build test -project CleanViewLayout.xcodeproj -scheme CleanViewLayout -destination 'platform=iOS Simulator,name=iPhone 7 Plus,OS=10.3.1'
-slather coverage --html --scheme CleanViewLayout CleanViewLayout.xcodeproj
+xcodebuild clean build test -workspace CleanViewLayout.xcworkspace -scheme CleanViewLayout -destination 'platform=iOS Simulator,name=iPhone 7 Plus,OS=10.3.1'
+# slather coverage -s --scheme CleanViewLayout --workspace CleanViewLayout.xcworkspace CleanViewLayout.xcodeproj
+slather coverage --html --scheme CleanViewLayout --workspace CleanViewLayout.xcworkspace CleanViewLayout.xcodeproj
